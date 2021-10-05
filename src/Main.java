1ffe8f1ed9cd9ef878264c72deb01a5cfa48d875
sperrsim <simon.sperr@htl-wels.at>
2021-10-05 10:11:33 +0200

    Implemented the Threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+/**
+ * @author Simon Sperr
+ * @version 2021.1.1, 05.10.2021
+ */
+
 public class Main {
     public static void main(String[] args) {
-        
+        //Creating Objects for the Threads to run
+        CountPrinter countPrinter = new CountPrinter();
+        DatePrinter datePrinter = new DatePrinter();
+
+        //Creating the Threads
+        Thread countThread = new Thread(countPrinter);
+        Thread dateThread = new Thread(datePrinter);
+
+        //Starting the Threads
+        countThread.start();
+        dateThread.start();
     }
 }
