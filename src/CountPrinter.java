0bd8644e16f8d8fa30dd09e7db2b7493f3723915
sperrsim <simon.sperr@htl-wels.at>
2021-10-05 10:14:01 +0200

    Changed Count to 2000

--- src/CountPrinter.java
+++ src/CountPrinter.java
@@ -6,7 +6,7 @@
 public class CountPrinter implements Runnable{
     @Override
     public void run() {
-        for (int i = 1; i <= 200; i++) {
+        for (int i = 1; i <= 2000; i++) {
             System.out.println(i);
         }
     }
